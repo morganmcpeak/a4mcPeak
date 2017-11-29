3087bfd3579e2492472e19cb48c51745df0f0530
Morgan McPeak <mcpeakmorgan@gmail.com>
2017-11-28 19:36:51 -0500

    Update form-component.js

--- madlibs/form-component.js
+++ madlibs/form-component.js
@@ -1,17 +1,17 @@
 (function(){
  var formComponent = {
-     controller: "PosController as pos",
+     controller: "PosController",
      template: `
      <form>
-     <input ng-model="pos.word.noun" placeholder="noun">
-     <input ng-model="pos.word.adjective" placeholder="adjective">
-     <input ng-model="pos.word.verb" placeholder="verb">
-     <input ng-model="pos.word.adjective2" placeholder="adjective">
-     <input ng-model="pos.word.noun2" placeholder = "noun">
-     <input ng-model="pos.word.place" placeholder="place">
-     <input ng-model="pos.word.passedtenseverb" placeholder="Past Tense Verb">
-     <input ng-model="pos.word.adverb" placeholder = "Adverb">
-     <button ng-click = "pos.enterWords(pos.word);"> <a href="#!/results">Enter Words</button></a>
+     <input ng-model="$ctrl.word.noun" placeholder="noun">
+     <input ng-model="$ctrl.word.adjective" placeholder="adjective">
+     <input ng-model="$ctrl.word.verb" placeholder="verb">
+     <input ng-model="$ctrl.word.adjective2" placeholder="adjective">
+     <input ng-model="$ctrl.word.noun2" placeholder = "noun">
+     <input ng-model="$ctrl.word.place" placeholder="place">
+     <input ng-model="$ctrl.word.passedtenseverb" placeholder="Past Tense Verb">
+     <input ng-model="$ctrl.word.adverb" placeholder = "Adverb">
+     <button ng-click = "$ctrl.enterWords($ctrl.word);"> <a href="#!/results">Enter Words</button></a>
      <form>
      `
  };
